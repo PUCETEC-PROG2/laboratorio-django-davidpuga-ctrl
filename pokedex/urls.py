25ccb1b175d7f04b3cab911d6dab0bdd754a7bdd
Sebastian <p@MacBook-Air-de-Sebastian.local>
2025-11-04 23:45:47 -0500

    Laboratorio 5

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -3,10 +3,13 @@ URL configuration for pokedex app.
 """
 from django.urls import path
 from . import views  # Importa las vistas de tu app
-
+app_name = 'pokedex'
 urlpatterns = [
     # Estas son las URLs de tu app
     path("", views.index, name="index"),
     path("trainer/<int:id>/", views.display_trainer, name="display_trainer"),
     path("<int:id>/", views.pokemon, name="pokemon"),
-]
+    path ("add_pokemon/", views.add_pokemon, name="add_pokemon"),
+    path("edit_pokemon/<int:pokemon_id>/", views.edit_pokemon, name="edit_pokemon"),
+    path("delete_pokemon/<int:pokemon_id>/", views.delete_pokemon, name="delete_pokemon"),
+]  
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Pokemon, Trainer
+from pokedex.forms import PokemonForm
+from django.shortcuts import redirect, render
 
 def index(request):
     pokemons = Pokemon.objects.all()
@@ -28,4 +30,31 @@ def display_trainer(request, id):
         'trainer': trainer
     }
     return HttpResponse(template.render(context, request))
+  
+def add_pokemon(request):
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm()
+    
+    return render(request,'pokemon_form.html',{'form':form})
 
+
+def edit_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(pk=pokemon_id)
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm(instance=pokemon)
+    return render(request, 'pokemon_form.html', {'form': form})
+
+def delete_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(pk=pokemon_id)
+    pokemon.delete()
+    return redirect('pokedex:index')
